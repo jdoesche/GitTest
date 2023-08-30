87a95e8ee725ce0e5fcfd75df768a311db83228a
Jesse Doescher <jdoesche@ut.utm.edu>
2023-08-30 11:33:57 -0500

    I think I broke it

--- main.cpp
+++ main.cpp
@@ -2,9 +2,35 @@
 #include <iostream>
 using namespace std;
 
+int recsum(int n);
+
 int main()
 {
   cout << "HELLO World!" << endl;
+
+  int n;
+  cout << "Please enter a number:";
+  cin >> n;
+  cout << endl;
+
+  int ret = recsum(n);
+
+  cout << "The recursive sum of " << n << " is " << ret << endl;
   
   return 0;
+}
+
+
+int recsum(n)
+{
+  int i = 1;
+  int sum = 0;
+  
+  while (i <= n)
+    {
+      sum += i;
+      i++;
+    }
+
+  return sum;
 }
